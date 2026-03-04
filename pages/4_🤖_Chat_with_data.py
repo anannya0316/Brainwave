d6b41d29eb5edb53502f28692fc17f58d198d616
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2026-03-04 08:40:37 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- pages/4_🤖_Chat_with_data.py
+++ pages/4_🤖_Chat_with_data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_chat import message
 from langchain_community.embeddings import SentenceTransformerEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
