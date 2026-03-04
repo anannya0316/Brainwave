bf0f068c6610f24c63bc50853de7046bed6345e2
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2026-03-04 08:57:18 +0530

    Update import for PromptTemplate from langchain

--- pages/3_🧐_Summarize_Docs.py
+++ pages/3_🧐_Summarize_Docs.py
@@ -2,7 +2,7 @@ import streamlit as st
 import asyncio
 from langchain_community.document_loaders import PyPDFLoader
 from PyPDF2 import PdfReader
-from langchain_core.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain_openai import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
